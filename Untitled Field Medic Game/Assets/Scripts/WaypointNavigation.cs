a83ae8752482ffc47b8e8e1c2ce12a3fca2445b7
mingosmingos <jdomingoscorreiab@gmail.com>
2026-05-28 13:26:14 +0100

    Adding trails

--- Untitled Field Medic Game/Assets/Scripts/WaypointNavigation.cs
+++ Untitled Field Medic Game/Assets/Scripts/WaypointNavigation.cs
@@ -7,9 +7,11 @@ public class WaypointNavigation : MonoBehaviour
 
 
     public float speed = 2f; // It should depend on the entities' speed stat.
+    public float rotationSpeed = 180f;
 
     private Transform[] waypoints;
     private int currentWaypointIndex = 0;
+    private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,6 +26,8 @@ public class WaypointNavigation : MonoBehaviour
         }
 
         waypoints = System.Array.ConvertAll(waypointObjects, wp => wp.transform);
+
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -35,12 +39,16 @@ public class WaypointNavigation : MonoBehaviour
 
         Vector3 direction = target.position - transform.position;
 
+        // Rotation
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
         float zRotation = angle + 90f;
-        transform.rotation = Quaternion.Euler(0f, 0f, zRotation);
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, zRotation);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        // transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+        Vector2 targetPos = Vector2.MoveTowards(rb.position, target.position, speed * Time.deltaTime);
+        rb.MovePosition(targetPos);
 
         if (Vector2.Distance(transform.position, target.position) < 0.1f)
         {
